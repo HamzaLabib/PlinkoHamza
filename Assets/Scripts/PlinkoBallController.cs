80e46642480625221d9c4883bd396981cbcf4dac
HamzaLabib <86428084+HamzaLabib@users.noreply.github.com>
2021-10-28 10:32:26 -0400

    some update

--- Assets/Scripts/PlinkoBallController.cs
+++ Assets/Scripts/PlinkoBallController.cs
@@ -4,12 +4,23 @@ using UnityEngine;
 
 public class PlinkoBallController : MonoBehaviour
 {
-    public float speed = 1f;
-    public Collider myCollider;
+    public float speed = 10f;
+    public Rigidbody rb;
+
+    private void Update()
+    {
+        GetControllers();
+    }
     void GetControllers()
     {
         if (Input.GetKey(KeyCode.RightArrow))
-            gameObject.GetComponent<Rigidbody>();
+            rb.AddForce(speed * Time.deltaTime, 0, 0, ForceMode.Impulse);
+        if (Input.GetKey(KeyCode.LeftArrow))
+            rb.AddForce(-speed * Time.deltaTime, 0, 0, ForceMode.Impulse);
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            rb.useGravity = true;
+            gameObject.GetComponent<PlinkoBallController>().enabled = false;
+        }
     }
-
 }
